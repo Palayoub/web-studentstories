7acf8d7d9ba968e75549b2efdf883def5e5d22b1
mohammedber <mohammedoujda16@gmail.com>
2017-02-28 00:30:46 +0100

    hada houwa li chouf

--- test_project/signup/models.py
+++ test_project/signup/models.py
@@ -6,12 +6,3 @@ class Student(models.Model):
     last_name = models.CharField(max_length=15)
     age=models.IntegerField(default=15)
     date_bith=models.DateTimeField()
-
-    print('Sign up now !')
-    name = input('Your Name: ')
-    email = input('Your Email: ')
-    password = input('Your Password: ')
-
-
-
-
