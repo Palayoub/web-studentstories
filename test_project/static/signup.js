8ca8a7bd5a0b251c34c725a2ef09607f6f091c52
Mohamed Ber <mohammedoujda16@gmail.com>
2017-06-18 19:47:45 +0200

    modif signup.css signup.js signup.html

---
+++ test_project/static/signup.js
@@ -0,0 +1,38 @@
+function changeColor() {
+	var a = document.getElementsByName("button");
+	for (var i = 0; i < a.length; i++) {
+		a[i].style.backgroundColor = '#4CAF50';
+	}
+}
+function changColor() {
+	var a = document.getElementsByName("button");
+	for (var i = 0; i < a.length; i++) {
+		a[i].style.backgroundColor = '#E48145';
+	}
+}
+
+function changeColor1() {
+	var a = document.getElementsByName("button1");
+	for (var i = 0; i < a.length; i++) {
+		a[i].style.backgroundColor = '#4CAF50';
+	}
+}
+function changColor1() {
+	var a = document.getElementsByName("button1");
+	for (var i = 0; i < a.length; i++) {
+		a[i].style.backgroundColor = '#E48145';
+	}
+}
+
+function changeColor2() {
+	var a = document.getElementsByName("button2");
+	for (var i = 0; i < a.length; i++) {
+		a[i].style.backgroundColor = '#4CAF50';
+	}
+}
+function changColor2() {
+	var a = document.getElementsByName("button2");
+	for (var i = 0; i < a.length; i++) {
+		a[i].style.backgroundColor = '#E48145';
+	}
+}
